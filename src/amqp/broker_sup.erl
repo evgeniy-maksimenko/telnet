53e3d2d4037788127a53699ed9666b609a5d0a98
user <evgenij.maksimenko.01@privatbank.ua>
2014-11-04 17:30:12 +0200

    a

--- src/amqp/broker_sup.erl
+++ src/amqp/broker_sup.erl
@@ -13,9 +13,9 @@ init([]) ->
   ets:new(logs, [named_table, public, set]),
 
   Flags = {one_for_one, 5, 10},
-  {ok, Connect} = amqp_connection:start(#amqp_params_network{host = proplists:get_value(host, RabbitMq)}),
-  {ok, Channel} = amqp_connection:open_channel(Connect),
+  %%{ok, Connect} = amqp_connection:start(#amqp_params_network{host = proplists:get_value(host, RabbitMq)}),
+  %%{ok, Channel} = amqp_connection:open_channel(Connect),
 
-  BrokerWorkerer = {broker_worker, {broker_worker, start_link, [Connect, Channel]}, permanent, 10500, worker, [broker_worker]},
-  ConsumerWorker = {consumer_worker, {consumer_worker, start_link, [Channel]}, permanent, 10500, worker, [consumer_worker]},
-  {ok, { Flags , [BrokerWorkerer, ConsumerWorker] }}.
+  %%BrokerWorkerer = {broker_worker, {broker_worker, start_link, [Connect, Channel]}, permanent, 10500, worker, [broker_worker]},
+  %%ConsumerWorker = {consumer_worker, {consumer_worker, start_link, [Channel]}, permanent, 10500, worker, [consumer_worker]},
+  {ok, { Flags , [] }}.
--- src/validate.erl
+++ src/validate.erl
@@ -1,15 +1,21 @@
 -module(validate).
 -export([date/2]).
+-export([convert/2]).
 
 -define(FORMAT_SIZE_BIT, 80).
-
+%% =============================================================================
+%% > validate:date(<<"2012-10-11">>, <<"YYYY-MM-DD">>).
+%% `~!@#$%^&*()_+{}[]"';:<>,./?|\
+%% YY YYYY MM dd
+%% =============================================================================
 -spec date(Date::binary(), Format::binary()) -> tuple() | {error, tuple()}.
-date(Date, Format) ->
+date(Date, Format) when (is_binary(Date) and is_binary(Format))  ->
   try
     date_vs_format(bit_size(Date) =:= bit_size(Format), Date, Format)
   catch
     _ : Reason -> {error, {failed, Reason}}
-  end.
+  end;
+date(Date, Format) -> {error, {failed, insert_data, Date, Format}}.
 
 -spec date_vs_format(boolean(), Date::binary(), Format::binary()) -> tuple() | {error, tuple()}.
 date_vs_format(true, Date, Format) ->
@@ -23,17 +29,25 @@ valid_size(?FORMAT_SIZE_BIT, Date) -> is_date(Date);
 valid_size(Size, _) -> {error,{failed, size, Size}}.
 
 -spec valid_dispatcher(boolean(), Y::boolean(),M::boolean(),D::boolean(), DateDispatcher::boolean()) -> tuple() | {error, tuple()}.
-valid_dispatcher(true, Y,M,D, DateDispatcher) -> {Y,M,D, DateDispatcher};
+valid_dispatcher(true, Y,M,D, _DateDispatcher) -> {binary_to_integer(Y),binary_to_integer(M), binary_to_integer(D)};
 valid_dispatcher(false, _Y,_M,_D, DateDispatcher) -> {error, {failed, dispatcher, DateDispatcher}}.
 
 -spec is_date(binary()) -> tuple().
 is_date(<<Y:32,DISAPTCHER:8,M:16,DISAPTCHER:8,D:16>>)->
   {<<Y:32>>,<<M:16>>,<<D:16>>,<<DISAPTCHER:8>>}.
 
-
-
-
-
-
-
+convert(Date, binary) ->
+  {{Y,M,D},{H,I,S}} = Date,
+  IntTolist =
+    integer_to_list(Y) ++ "-" ++
+    integer_to_list(M) ++ "-" ++
+    integer_to_list(D) ++ " " ++
+    integer_to_list(H) ++ ":" ++
+    integer_to_list(I) ++ ":" ++
+    integer_to_list(S) ,
+  list_to_binary(IntTolist);
+convert(Date, datetime) -> Date;
+convert(Date, unixtime) ->
+  {{Y,M,D},{H,I,S}} = Date,
+  calendar:datetime_to_gregorian_seconds({{Y, M, D}, {H, I, S}}).
 
